a275014787b1da6ba77089f988396576d172fa15
Samuel Davis <samuel.vertucc@gmail.com>
2019-03-01 14:52:40 +0000

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Samuel Davis on 01/03/2019.
+//  Copyright © 2019 Samuel Davis. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+}
